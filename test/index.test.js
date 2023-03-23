e3099550ebbf7fa349936de6f45a2995bcf82c00
Yash Nale <yashn@sahaj.ai>
2023-03-23 12:40:19 +0530

    Add tests for anchor tags

--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,28 @@
 import { JSDOM } from "jsdom";
+const { getByRole } = require("@testing-library/dom");
 import fs from "fs";
 
-const dom = new JSDOM(
-  "<!DOCTYPE html><html><head></head><body></body></html>",
-  {
-    url: "http://localhost",
-  }
-);
-
+const dom = new JSDOM("<!DOCTYPE html><html><head></head><body></body></html>");
 const { window } = dom;
 
+const { document } = window;
+global.document = document;
+
 const html = fs.readFileSync("./src/index.html", "utf-8");
-window.document.documentElement.innerHTML = html;
+document.documentElement.innerHTML = html;
 
 describe("Index", () => {
   test("title element contains correct text", () => {
-    expect(window.document.title).toBe("ESOP Trading");
+    expect(document.title).toBe("ESOP Trading");
+  });
+
+  test("placeOrder element contains correct link", () => {
+    const placeOrder = getByRole(document, "link", { name: "Place Order" });
+    expect(placeOrder.getAttribute("href")).toBe("./html/PlaceOrder.html");
+  });
+
+  test("orderHistory element contains correct link", () => {
+    const placeOrder = getByRole(document, "link", { name: "Order History" });
+    expect(placeOrder.getAttribute("href")).toBe("./html/OrderHistory.html");
   });
 });
